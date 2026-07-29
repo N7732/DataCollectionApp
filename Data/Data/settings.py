aba2cd8917882a3760f4f5a8c36c4514c7f99ead
N7732 <oliviernshimyumuremyi4@gmail.com>
2026-07-29 15:57:58 +0200

    Allow X-API-KEY header in CORS settings

--- Data/Data/settings.py
+++ Data/Data/settings.py
@@ -149,6 +149,16 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
 
+CORS_ALLOW_HEADERS = (
+    "accept",
+    "authorization",
+    "content-type",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+    "x-api-key",
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
