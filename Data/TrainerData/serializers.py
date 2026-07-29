b6cadf0d89f058ef1bb05c3a3716e305714fa3e8
N7732 <oliviernshimyumuremyi4@gmail.com>
2026-07-29 16:27:10 +0200

    Fix Dashboards

--- Data/TrainerData/serializers.py
+++ Data/TrainerData/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from .models import Trainer
 
 class TrainerSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source='user.first_name', read_only=True)
+    email = serializers.EmailField(source='user.email', read_only=True)
+    
     class Meta:
         model = Trainer
         fields = '__all__'
